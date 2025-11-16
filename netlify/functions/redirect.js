f68a1ca70912f056822a732626e3074f4e194f33
vishalearning <mrvishalsingh49@gmail.com>
2025-11-16 12:15:04 +0530

    Update redirect.js

--- netlify/functions/redirect.js
+++ netlify/functions/redirect.js
@@ -3,13 +3,6 @@ export async function handler(event, context) {
     const MAX_WHATSAPP = 10;
 
     const encodedLinks = [
-      "aHR0cHM6Ly93YS5tZS85MTgwMDY4NTU5MzE=",
-      "aHR0cHM6Ly93YS5tZS85MTkwNDc0OTk1OTI=",
-      "aHR0cHM6Ly93YS5tZS85MTc5ODMzMjQ3MTQ=",
-      "aHR0cHM6Ly93YS5tZS85MTg5ODQ2OTYxNTE=",
-      "aHR0cHM6Ly93YS5tZS85MTk5ODY3Mjg3NDA=",
-      "aHR0cHM6Ly93YS5tZS85MTkwMjQ3OTg3NzA=",
-      "aHR0cHM6Ly93YS5tZS85MTc1ODQwMTk3NDg=",
 
       "aHR0cHM6Ly93YS5tZS85MTcwOTMwNDExMDU=",
       "aHR0cHM6Ly93YS5tZS85MTkwMTI2NTM5NDc=",
